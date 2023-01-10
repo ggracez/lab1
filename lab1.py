f9c46399afa186f9c31fc9a47989b2a9a51bdd8c
ggracez <gracezhu13@gmail.com>
2023-01-09 18:06:16 -0700

    modified script

--- lab1.py
+++ lab1.py
@@ -3,3 +3,5 @@ import requests
 print(requests.__version__)
 
 print(requests.get("http://www.google.com"))
+
+print(requests.get("https://raw.githubusercontent.com/ggracez/lab1/master/lab1.py").text)
